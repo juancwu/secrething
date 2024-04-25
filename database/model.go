6299d0591ffc9dad38c765ee47b736431c34ebb5
jc <46619361+juancwu@users.noreply.github.com>
2024-04-25 17:26:23 -0400

    update user model

--- database/model.go
+++ database/model.go
@@ -1,11 +1,16 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	ID        uint `gorm:"primaryKey"`
-	Name      string
+	ID        uint   `gorm:"primaryKey"`
 	Email     string `gorm:"unique"`
 	PublicKey string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
