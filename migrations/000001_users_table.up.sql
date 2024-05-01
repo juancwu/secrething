a5c499b4d1b7753a66b2bd2804eb75555dc2a142
jc <46619361+juancwu@users.noreply.github.com>
2024-05-01 01:32:45 -0400

    update user table migration script to include pgcrypto

--- migrations/000001_users_table.up.sql
+++ migrations/000001_users_table.up.sql
@@ -1,7 +1,9 @@
+CREATE EXTENSION pgcrypto;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) DEFAULT '',
     last_name VARCHAR(255) DEFAULT '',
     email TEXT UNIQUE NOT NULL,
-    pem_public_key TEXT NOT NULL
+    pem_public_key BYTEA -- stored encrypted public key
 );
