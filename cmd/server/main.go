95316a8a4ce30e8924949d684c6565134a7c6ac8
jc <46619361+juancwu@users.noreply.github.com>
2024-05-02 18:17:38 -0400

    remove unused code in server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"net/http"
 	"os"
 
 	"github.com/charmbracelet/log"
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
-	// "github.com/labstack/echo/v4/middleware"
 
 	"github.com/juancwu/konbini/server/database"
 	_ "github.com/juancwu/konbini/server/env"
@@ -30,20 +25,6 @@ func (rq *ReqValidator) Validate(i interface{}) error {
 	return nil
 }
 
-func loadPublicKey(pemData []byte) (*rsa.PublicKey, error) {
-	block, _ := pem.Decode(pemData)
-	if block == nil {
-		return nil, errors.New("Failed to decode PEM block")
-	}
-
-	publicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-	if err != nil {
-		return nil, err
-	}
-
-	return publicKey.(*rsa.PublicKey), nil
-}
-
 func main() {
 	/*
 	   Environemnt variables are loaded in the env package when it is imported
@@ -53,7 +34,6 @@ func main() {
 	database.Migrate()
 
 	e := echo.New()
-	// e.Use(middleware.Logger())
 	e.Validator = &ReqValidator{validator: validator.New()}
 
 	router.SetupAuthRoutes(e)
