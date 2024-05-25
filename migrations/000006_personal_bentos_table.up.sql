560e0ae049eb6c8e7a2c15950cc5ba45ae36fe1d
jc <46619361+juancwu@users.noreply.github.com>
2024-05-25 00:10:14 -0400

    update migrations

--- migrations/000006_personal_bentos_table.up.sql
+++ migrations/000006_personal_bentos_table.up.sql
@@ -3,8 +3,8 @@ CREATE TABLE personal_bentos (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     owner_id UUID,
-    content BYTEA NOT NULL, -- encrypted
-    pub_key BYTEA NOT NULL, -- encrypted
+    content TEXT NOT NULL,
+    pub_key TEXT NOT NULL,
 
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
