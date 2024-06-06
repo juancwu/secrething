90a0f12403db6989f06e4cc58d9d10ce0f9c7214
jc <46619361+juancwu@users.noreply.github.com>
2024-06-06 17:16:35 -0400

    add create users table migration

---
+++ migrations/20240606193649_users_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL DEFAULT '',
+    last_name VARCHAR(50) NOT NULL DEFAULT '',
+    password TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    email_verified BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+DROP EXTENSION "uuid-ossp";
+-- +goose StatementEnd
