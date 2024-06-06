c520fe6ea8206562280f6476b30dd119c2dee938
jc <46619361+juancwu@users.noreply.github.com>
2024-06-06 17:15:03 -0400

    add store connection fn

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	// package modules
+	"os"
+
 	"go.uber.org/zap"
 
 	// custom modules
 	"github.com/juancwu/konbini/config"
+	"github.com/juancwu/konbini/store"
 )
 
 func main() {
@@ -14,5 +17,12 @@ func main() {
 		logger, _ := zap.NewProduction()
 		defer logger.Sync()
 		logger.Fatal("Failed to load env", zap.Error(err))
+	}
+
+	err = store.Connect(os.Getenv("DB_URL"))
+	if err != nil {
+		logger, _ := zap.NewProduction()
+		defer logger.Sync()
+		logger.Fatal("Failed to establish connection with database", zap.Error(err))
 	}
 }
