3afb3ba613e4f8804bf4344f6c0067f381be45e0
jc <46619361+juancwu@users.noreply.github.com>
2024-07-09 00:11:42 -0400

    add protect middleware
    this middleware protects adds a layer of authorization to routes

---
+++ middleware/protect.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/juancwu/konbini/jwt"
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	// JWT_CLAIMS is a key to get the claims parsed by the Protect middleware in echo.Context.
+	JWT_CLAIMS = "JWT_CLAIMS"
+)
+
+// Protect is a middleware that is used to protect a route by authorizing clients with a Bearer token.
+//
+// This middleware will attach the claims into the echo.Context using JWT_CLAIMS.
+func Protect() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			requestId := c.Request().Header.Get(echo.HeaderXRequestID)
+			path := c.Request().URL.Path
+			authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
+			if authHeader == "" {
+				log.Error().Str(echo.HeaderXRequestID, requestId).Str("path", path).Msg("Missing authorization header to access route.")
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "unauthorized"})
+			}
+			parts := strings.Split(authHeader, " ")
+			if len(parts) < 2 || strings.ToLower(parts[0]) != "bearer" {
+				log.Error().Str(echo.HeaderXRequestID, requestId).Str("path", path).Msg("Invalid authorization header.")
+				return c.JSON(http.StatusBadRequest, map[string]string{"message": "Invalid authorization header. Only bearer token is supported."})
+			}
+			// verify jwt
+			token, err := jwt.VerifyAccessToken(parts[1])
+			if err != nil {
+				log.Error().Err(err).Str(echo.HeaderXRequestID, requestId).Str("path", path).Msg("Failed to validate access token.")
+				return c.JSON(http.StatusUnauthorized, map[string]string{"message": "unauthorized"})
+			}
+			c.Set(JWT_CLAIMS, token.Claims)
+			return next(c)
+		}
+	}
+}
