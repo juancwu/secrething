6f5b11a40dfc61a0e92c5a63fce6903e1dd47687
jc <46619361+juancwu@users.noreply.github.com>
2024-11-12 15:42:18 -0500

    setup log

---
+++ common/time_formats.go
@@ -0,0 +1,5 @@
+package common
+
+const (
+	FRIENDLY_TIME_FORMAT = "2006-01-02 15:04:05 MST"
+)
--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"konbini/common"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = common.FRIENDLY_TIME_FORMAT
+	if os.Getenv("APP_ENV") != "production" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: common.FRIENDLY_TIME_FORMAT})
+	}
+}
