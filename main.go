e4f52e3293b201165e07e2f03a84c93f20c801f9
jc <46619361+juancwu@users.noreply.github.com>
2024-11-12 15:43:49 -0500

    load .env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"konbini/common"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -12,5 +13,9 @@ func main() {
 	zerolog.TimeFieldFormat = common.FRIENDLY_TIME_FORMAT
 	if os.Getenv("APP_ENV") != "production" {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: common.FRIENDLY_TIME_FORMAT})
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to load .env")
 	}
 }
