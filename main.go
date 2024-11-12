21090deada0ea8a5a9c39ccdd618f13ce75eb503
Jc <46619361+juancwu@users.noreply.github.com>
2024-11-12 15:50:30 -0500

    Revert "setup log"
    This reverts commit d2e2368cc0db97dd323bdc4fbf1da27c36578037.

--- main.go
+++ main.go
@@ -1,16 +1,3 @@
 package main
 
-import (
-	"konbini/common"
-	"os"
-
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
-)
-
-func main() {
-	zerolog.TimeFieldFormat = common.FRIENDLY_TIME_FORMAT
-	if os.Getenv("APP_ENV") != "production" {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: common.FRIENDLY_TIME_FORMAT})
-	}
-}
+func main() {}
