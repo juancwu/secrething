1f24181d32429909c89a96fc025a9d21d7a12523
Jc <46619361+juancwu@users.noreply.github.com>
2024-11-12 15:50:30 -0500

    Revert "load .env"
    This reverts commit e4f52e3293b201165e07e2f03a84c93f20c801f9.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"konbini/common"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -13,9 +12,5 @@ func main() {
 	zerolog.TimeFieldFormat = common.FRIENDLY_TIME_FORMAT
 	if os.Getenv("APP_ENV") != "production" {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: common.FRIENDLY_TIME_FORMAT})
-	}
-
-	if err := godotenv.Load(); err != nil {
-		log.Fatal().Err(err).Msg("Failed to load .env")
 	}
 }
