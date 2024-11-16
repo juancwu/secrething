3c857ca0e4ece1195d45c55d1e0678aefbe2abb6
jc <46619361+juancwu@users.noreply.github.com>
2024-11-15 21:28:38 -0500

    add logger middleware

---
+++ middleware/logger.go
@@ -0,0 +1,69 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func Logger() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			res := c.Response()
+			start := time.Now()
+
+			err := next(c)
+			var logger *zerolog.Event
+			if err != nil {
+				logger = log.Error().Err(err)
+				// gotta let the error handler run to get the response data
+				c.Error(err)
+			} else {
+				logger = log.Info()
+			}
+
+			latency := time.Now().Sub(start)
+			protocol := req.Proto
+			remoteIP := c.RealIP()
+			host := req.Host
+			method := req.Method
+			uri := req.RequestURI
+			uriPath := req.URL.Path
+			routePath := c.Path()
+			requestID := req.Header.Get(echo.HeaderXRequestID)
+			referer := req.Referer()
+			userAgent := req.UserAgent()
+			status := res.Status
+			reqContentType := req.Header.Get(echo.HeaderContentType)
+			reqContentLength := req.Header.Get(echo.HeaderContentLength)
+			resSize := res.Size
+			resContentType := res.Header().Get(echo.HeaderContentType)
+			resContentLength := res.Header().Get(echo.HeaderContentLength)
+
+			logger.
+				Dur("latency", latency).
+				Str("protocol", protocol).
+				Str("remote_ip", remoteIP).
+				Str("host", host).
+				Str("method", method).
+				Str("uri", uri).
+				Str("uri_path", uriPath).
+				Str("route_path", routePath).
+				Str("request_id", requestID).
+				Str("referer", referer).
+				Str("user_agent", userAgent).
+				Int("status", status).
+				Str("request_content_type", reqContentType).
+				Str("request_content_length", reqContentLength).
+				Int64("response_size", resSize).
+				Str("response_content_type", resContentType).
+				Str("response_content_length", resContentLength).
+				Send()
+
+			return err
+		}
+	}
+}
