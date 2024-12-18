c70a5282a576af0d33bdc0e203b62033b00647f2
jc <46619361+juancwu@users.noreply.github.com>
2024-12-18 16:05:12 -0500

    add access_logs table

--- konbini-server/migrations/schema.sql
+++ konbini-server/migrations/schema.sql
@@ -101,3 +101,13 @@ CREATE TABLE bento_tokens(
     last_used_at TEXT,
     expires_at TEXT
 );
+CREATE TABLE access_logs (
+    id TEXT NOT NULL PRIMARY KEY DEFAULT (uuid4()),
+    user_id TEXT,
+    bento_id TEXT,
+    group_id TEXT,
+    bento_token_id TEXT,
+    action TEXT NOT NULL,
+    details JSONB,
+    accessed_at TEXT NOT NULL DEFAULT (datetime('now', 'utc'))
+);
