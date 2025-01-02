d4eddcef118ecbb6b3f550b64e8289a20f56fda3
jc <46619361+juancwu@users.noreply.github.com>
2025-01-01 22:22:39 -0500

    add RandomDigits method

---
+++ server/test/rand_test.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"konbini/server/utils"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRandomDigits(t *testing.T) {
+	t.Run("sequence of 6", func(t *testing.T) {
+		seq, err := utils.RandomDigits(6)
+		require.NoError(t, err)
+		require.Equal(t, 6, len(seq))
+	})
+}
