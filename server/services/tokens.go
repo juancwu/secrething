feb7e1f21fdf8da4e47be93c740be59258c367b0
jc <46619361+juancwu@users.noreply.github.com>
2025-01-05 12:20:53 -0500

    add method to create a new jwt

--- server/services/tokens.go
+++ server/services/tokens.go
@@ -2,10 +2,12 @@ package services
 
 import (
 	"encoding/base64"
+	"errors"
 	"konbini/server/config"
 	"konbini/server/utils"
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
@@ -13,7 +15,58 @@ const (
 	FULL_USER_TOKEN_TYPE    string = "full_user_token"
 	PARTIAL_USER_TOKEN_TYPE string = "partial_user_token"
 	EMAIL_TOKEN_TYPE        string = "email_token"
+
+	customer string = "customer"
 )
+
+var (
+	ErrInvalidTokenType error = errors.New("Invalid token type. Use constants to not make a mistake.")
+)
+
+type JWTClaims struct {
+	Type string `json:"type"`
+	jwt.RegisteredClaims
+}
+
+// NewJWT generates a new JWT and signs it with HS256.
+// An id must be provided since it is what relates the JWT to the
+// row stored in the database.
+func NewJWT(id, tokType string, expiresAt time.Time) (string, error) {
+	cfg, err := config.Global()
+	if err != nil {
+		return "", err
+	}
+
+	claims := JWTClaims{
+		Type: tokType,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "Konbini",
+			ID:        id,
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(expiresAt),
+			Audience:  []string{customer},
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	key, err := getJWTKeyByType(tokType, cfg)
+	if err != nil {
+		return "", err
+	}
+	return token.SignedString(key)
+}
+
+func getJWTKeyByType(tokType string, cfg *config.Config) ([]byte, error) {
+	switch tokType {
+	case FULL_USER_TOKEN_TYPE:
+		return cfg.GetFullTokenKey(), nil
+	case PARTIAL_USER_TOKEN_TYPE:
+		return cfg.GetPartialTokenKey(), nil
+	}
+
+	return nil, ErrInvalidTokenType
+}
 
 type EmailToken struct {
 	Id        string
