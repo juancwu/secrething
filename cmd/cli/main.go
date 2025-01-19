0ef194feb7ab29118fcdb33c617a65951ef0f488
jc <46619361+juancwu@users.noreply.github.com>
2025-01-19 01:36:09 -0500

    run tui when no arguments are passed

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
 	command "konbini/cli/commands"
+	"konbini/cli/models"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	err := command.Execute()
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) == 1 {
+		m := models.NewModel()
+		p := tea.NewProgram(m, tea.WithAltScreen())
+		_, err := p.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		err := command.Execute()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
