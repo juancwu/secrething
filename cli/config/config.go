e499f50efa350db82a2a2841feab9aacf6a1f9f0
jc <46619361+juancwu@users.noreply.github.com>
2025-01-22 13:34:35 -0500

    store term size on config

--- cli/config/config.go
+++ cli/config/config.go
@@ -2,6 +2,13 @@ package config
 
 var backendUrl string
 
+type terminal struct {
+	width  int
+	height int
+}
+
+var t terminal
+
 func Init() {
 	if backendUrl == "" {
 		backendUrl = "http://localhost:3000/api/v1"
@@ -10,4 +17,23 @@ func Init() {
 
 func BackendUrl() string {
 	return backendUrl
+}
+
+func TermWidth() int {
+	return t.width
+}
+
+func TermHeight() int {
+	return t.height
+}
+
+func TermSize() (width int, height int) {
+	width = t.width
+	height = t.height
+	return
+}
+
+func UpdateTermSize(width int, height int) {
+	t.width = width
+	t.height = height
 }
