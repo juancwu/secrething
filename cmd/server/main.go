2f22b71b77b6b0589dde2c36789bea9069871a6b
jc <46619361+juancwu@users.noreply.github.com>
2025-03-02 01:46:30 -0500

    reset cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,44 +1,16 @@
 package main
 
 import (
-	"github.com/juancwu/konbini/server/config"
-	"github.com/juancwu/konbini/server/db"
-	"github.com/juancwu/konbini/server/handlers"
-	"github.com/juancwu/konbini/server/routes"
-	inner_validator "github.com/juancwu/konbini/server/validator"
+	"os"
 
-	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	cfg, err := config.New()
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to load server configuration")
-	}
-
-	dbUrl, dbAuthToken := cfg.GetDatabaseConfig()
-
 	e := echo.New()
 
-	validate := validator.New()
-	cv := inner_validator.Validator{Validator: validate}
-	e.Validator = &cv
-
-	// set global error handler
-	e.HTTPErrorHandler = handlers.ErrorHandler()
-
-	// v1 routes
-	apiV1 := e.Group("/api/v1")
-	routeConfig := &routes.RouteConfig{
-		Echo:         apiV1,
-		ServerConfig: cfg,
-		DBConnector:  db.NewConnector(dbUrl, dbAuthToken),
-	}
-	routes.SetupRoutesV1(routeConfig)
-
-	err = e.Start(cfg.GetPort())
+	err := e.Start(os.Getenv("PORT"))
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to start server.")
 	}
