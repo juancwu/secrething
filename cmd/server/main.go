0ef40a85b1975077a6b766afd62c403c8f3c36a4
jc <46619361+juancwu@users.noreply.github.com>
2025-03-03 22:52:50 -0500

    update server/main.go to new structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/juancwu/konbini/server/db"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to load .env")
+	}
+
+	// Establish database connection
+	tursoConnector := db.NewTursoConnector(os.Getenv("DATABASE_URL"), os.Getenv("DATABASE_AUTH_TOKEN"))
+	_, err := tursoConnector.Connect()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to establish connection with database")
+	}
+
 	e := echo.New()
 
-	err := e.Start(os.Getenv("PORT"))
-	if err != nil {
+	if err := e.Start(":" + os.Getenv("PORT")); err != nil {
 		log.Fatal().Err(err).Msg("Failed to start server.")
 	}
 }
