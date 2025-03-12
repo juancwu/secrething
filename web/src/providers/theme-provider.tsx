e09a14f8401f39e1fdf561bc142d07fd0a0f378d
jc <46619361+juancwu@users.noreply.github.com>
2025-03-12 01:27:38 -0400

    force light theme only

--- web/src/providers/theme-provider.tsx
+++ web/src/providers/theme-provider.tsx
@@ -14,7 +14,7 @@ type ThemeProviderState = {
 };
 
 const initialState: ThemeProviderState = {
-	theme: "system",
+	theme: "light",
 	setTheme: () => null,
 };
 
@@ -22,7 +22,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
 	children,
-	defaultTheme = "system",
+	defaultTheme = "light",
 	storageKey = "vite-ui-theme",
 	...props
 }: ThemeProviderProps) {
@@ -35,15 +35,15 @@ export function ThemeProvider({
 
 		root.classList.remove("light", "dark");
 
-		if (theme === "system") {
-			const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-				.matches
-				? "dark"
-				: "light";
-
-			root.classList.add(systemTheme);
-			return;
-		}
+		// if (theme === "system") {
+		// 	const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+		// 		.matches
+		// 		? "dark"
+		// 		: "light";
+		//
+		// 	root.classList.add(systemTheme);
+		// 	return;
+		// }
 
 		root.classList.add(theme);
 	}, [theme]);
