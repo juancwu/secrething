1e3d58f5b7ac594f6072dd46ae1daeeb32cc3e8c
jc <46619361+juancwu@users.noreply.github.com>
2025-03-12 01:27:30 -0400

    fix wrong landing page import

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Landing from "@/pages/landing";
+import Landing from "@/pages/Landing";
 
 export const Route = createFileRoute("/")({
 	component: Landing,
