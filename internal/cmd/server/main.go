032ef17c6f5a299a5da487437cce7d8d37b5b0f2
jc <46619361+juancwu@users.noreply.github.com>
2025-03-27 21:31:46 -0400

    start echo server in internal/cmd/server

--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"github.com/juancwu/konbini/internal/server/config"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	if err := config.Load(".env"); err != nil {
+		panic(err)
+	}
+
+	e := echo.New()
+	e.HideBanner = !config.IsDevelopment()
+
+	if err := e.Start(config.Server().Address); err != nil {
 		panic(err)
 	}
 }
