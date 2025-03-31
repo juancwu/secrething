939a8fd20478c2d9442264bfff98d66a331fc586
jc <46619361+juancwu@users.noreply.github.com>
2025-03-31 18:39:55 -0400

    reorganize permissions module

--- internal/server/permissions/team.go
+++ internal/server/permissions/team.go
@@ -1,1 +1,15 @@
 package permissions
+
+type TeamPermission uint64
+
+const (
+	// Team permissions
+	PermissionTeamMembersRead   TeamPermission = 0x0000_0000_0000_0010
+	PermissionTeamMembersUpdate TeamPermission = 0x0000_0000_0000_0020
+	PermissionTeamMembersCreate TeamPermission = 0x0000_0000_0000_0040
+	PermissionTeamMembersDelete TeamPermission = 0x0000_0000_0000_0080
+	PermissionTeamVaultRead     TeamPermission = 0x0000_0000_0000_0100
+	PermissionTeamVaultUpdate   TeamPermission = 0x0000_0000_0000_0200
+	PermissionTeamVaultCreate   TeamPermission = 0x0000_0000_0000_0400
+	PermissionTeamVaultDelete   TeamPermission = 0x0000_0000_0000_0800
+)
--- internal/server/permissions/vault.go
+++ internal/server/permissions/vault.go
@@ -1,1 +1,11 @@
 package permissions
+
+type VaultPermission uint64
+
+const (
+	// Vault permissions
+	PermissionVaultRead   VaultPermission = 0x0000_0000_0000_0001
+	PermissionVaultUpdate VaultPermission = 0x0000_0000_0000_0002
+	PermissionVaultCreate VaultPermission = 0x0000_0000_0000_0004
+	PermissionVaultDelete VaultPermission = 0x0000_0000_0000_0008
+)
