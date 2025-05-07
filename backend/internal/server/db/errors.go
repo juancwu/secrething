0ac877c04fc3c55fa4758ed91212608127478f93
jc <46619361+juancwu@users.noreply.github.com>
2025-05-07 13:08:53 -0400

    hard reset

--- backend/internal/server/db/errors.go
+++
@@ -1,9 +0,0 @@
-package db
-
-// IsNoRows checks whether the given error is a no rows returned error
-func IsNoRows(err error) bool {
-	if err == nil {
-		return false
-	}
-	return err.Error() == "sql: no rows in result set"
-}
