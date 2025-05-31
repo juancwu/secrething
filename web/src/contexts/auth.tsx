fd757f22c9c99be46c2234bfa2ff6f43a3d1bb3f
jc <46619361+juancwu@users.noreply.github.com>
2025-05-31 01:37:14 -0400

    [WEB] use ref to store auth token

--- web/src/contexts/auth.tsx
+++ web/src/contexts/auth.tsx
@@ -2,7 +2,7 @@ import { signin, signout, signup } from "@/lib/api/auth";
 import type { SigninBody, SignupBody } from "@/lib/api/types";
 import type { User } from "@/lib/types/auth";
 import type { ReactNode } from "@tanstack/react-router";
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext, useRef, useState } from "react";
 import { createContext, useMemo } from "react";
 import { flushSync } from "react-dom";
 
@@ -13,27 +13,29 @@ export type AuthContextValue = {
 	signin?: typeof signin;
 	signout?: typeof signout;
 	user?: User;
-	token?: string;
+	getAuthToken: () => string | undefined;
 	expiresAt?: number;
 };
 
 export const AuthContext = createContext<AuthContextValue>({
 	isLoading: true,
 	isAuthenticated: false,
+	getAuthToken: () => undefined,
 });
 
 export function AuthProvider({ children }: { children: ReactNode }) {
 	const [isLoading, _setIsLoading] = useState(true);
 	const [user, setUser] = useState<User | undefined>();
-	const [token, setToken] = useState<string | undefined>();
 	const [expiresAt, setExpiresAt] = useState<number | undefined>();
-	const isAuthenticated = !!user;
+	// NOTE: Using a ref to not cause re-render when the auth token refreshes
+	//       The auth token acts as an access token.
+	const authTokenRef = useRef<string | undefined>(undefined);
 
 	const handleSignup = useCallback(async (body: SignupBody) => {
 		const res = await signup(body);
+		authTokenRef.current = res.token;
 		flushSync(() => {
 			setUser(res.user);
-			setToken(res.token);
 			setExpiresAt(res.expires_at);
 		});
 		return res;
@@ -41,9 +43,9 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
 	const handleSignin = useCallback(async (body: SigninBody) => {
 		const res = await signin(body);
+		authTokenRef.current = res.token;
 		flushSync(() => {
 			setUser(res.user);
-			setToken(res.token);
 			setExpiresAt(res.expires_at);
 		});
 		return res;
@@ -51,9 +53,9 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
 	const handleSignout = useCallback(async () => {
 		const res = await signout();
+		authTokenRef.current = undefined;
 		flushSync(() => {
 			setUser(undefined);
-			setToken(undefined);
 			setExpiresAt(undefined);
 		});
 		return res;
@@ -62,24 +64,15 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 	const value = useMemo<AuthContextValue>(() => {
 		return {
 			isLoading,
-			isAuthenticated,
+			isAuthenticated: !!user,
 			user,
-			token,
 			expiresAt,
 			signup: handleSignup,
 			signin: handleSignin,
 			signout: handleSignout,
+			getAuthToken: () => authTokenRef.current,
 		} satisfies AuthContextValue;
-	}, [
-		isLoading,
-		isAuthenticated,
-		user,
-		token,
-		expiresAt,
-		handleSignin,
-		handleSignup,
-		handleSignout,
-	]);
+	}, [isLoading, user, expiresAt, handleSignin, handleSignup, handleSignout]);
 
 	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
